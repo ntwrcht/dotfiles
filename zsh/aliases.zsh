b62c62a43525f49f5facafb1a083c18a75b22cca
Canvas-xxx <s.worachote@hotmail.com>
2026-05-09 16:08:13 +0700

    chore: add recommended productivity aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,27 +1,53 @@
-# Aliases
-alias lc="eza -la --icons --git --group-directories-first"
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/.git/ --work-tree=$HOME'
+# --- Navigation ---
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias ~="cd ~"
+
+# --- Modern Replacements ---
+alias cat="bat"
+alias help="tldr"
 alias vim="nvim"
 alias vi="nvim"
 alias grep="rg"
 alias search="ddgr"
 alias nnn="nnn -dea"
 alias lg="lazygit"
+alias l="eza -1 --icons --group-directories-first"
+alias lc="eza -la --icons --git --group-directories-first"
+
+# --- Git Productivity ---
+alias g="git"
+alias gs="git status -sb"
+alias ga="git add"
+alias gc="git commit -v"
+alias gp="git pull"
+alias gpush="git push"
 alias gf='git fetch --all --prune'
 alias gb='git branch -a'
 alias gbm='git branch --merged'
 alias gbnm='git branch --no-merged'
 alias gbd='git branch --merged | grep -v "\*" | grep -v "main\|master" | xargs -n 1 git branch -D'
-alias gp="git pull"
 alias gl="git log --pretty=oneline"
+alias grb="git_rebase_dynamic"
 
-# Functions
+# --- System & Utilities ---
+alias reload="source ~/.zshrc"
+alias myip="curl -s ifconfig.me && echo"
+alias rm="rm -i" # Safety first
+alias ports="sudo lsof -iTCP -sTCP:LISTEN -P -n"
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/.git/ --work-tree=$HOME'
+
+# --- Containers ---
+alias d="docker"
+alias dc="docker-compose"
+
+# --- Functions ---
 git_rebase_dynamic() {
     git rebase -i HEAD~$1
 }
-alias grb="git_rebase_dynamic"
 
-# Tool-specific initialization
+# --- Tool Initialization ---
 if command -v zoxide >/dev/null 2>&1; then
   eval "$(zoxide init zsh)"
 fi
